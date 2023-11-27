87243472cbc685c3f4b9892eba93b61f7d6689e1
mrquaketotheworld <newrushiflyer@gmail.com>
2023-11-27 22:45:36 +0300

    Add 2 solutions

---
+++ solutions/helloworld.go
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+package solutions
+
+func greet() string {
+	return "hello world!"
+}
+
---
+++ solutions/stringtoarray.go
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+package solutions
+
+import "strings"
+
+func StringToArray(str string) []string {
+	return strings.Split(str, " ")
+}
